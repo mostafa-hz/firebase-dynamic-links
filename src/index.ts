8e225b08e0aa3454ba9026d51bdf339530d2d277
Mostafa <forcemos@gmail.com>
2020-09-10 12:14:27 +0430

    Export Parameters Types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
+import { AndroidInfo } from './types/android-info';
+import { IosInfo } from './types/ios-info';
+import { AnalyticsInfo } from './types/analytics-info';
+import { DynamicLinkInfo } from './types/dynamic-link-info';
+import { GooglePlayAnalytics } from './types/google-play-analytics';
+import { ItunesConnectAnalytics } from './types/itunes-connect-analytics';
+import { NavigationInfo } from './types/navigation-info';
+import { ShortLinkResponse } from './types/short-link-response';
+import { SocialMetaTagInfo } from './types/social-meta-tag-info';
+
 export { FirebaseDynamicLinks } from './firebase-dynamic-links';
+
+export {
+  AndroidInfo,
+  IosInfo,
+  AnalyticsInfo,
+  DynamicLinkInfo,
+  GooglePlayAnalytics,
+  ItunesConnectAnalytics,
+  NavigationInfo,
+  ShortLinkResponse,
+  SocialMetaTagInfo,
+};
